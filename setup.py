7ead3a8b598686947cc20cd09ec15734e41b63b9
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2021-05-06 17:11:04 +0200

    add scripts to setup.py, change stats script name

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/stefanocampanella/correlation-detector",
     packages=setuptools.find_packages(),
+    scripts=['bin/correlation-detector', 'bin/detections-stats'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
