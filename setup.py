083e306da83ac822a3ca47eb848a185f1d45c521
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2022-07-07 13:12:32 +0200

    Rename package, minor cleanup and refactor

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="correlation-detector",
+    name="eqtm",
     version="1.5.0",
     author="Stefano Campanella",
     author_email="scampanella@inogs.it",
     description="Earthquake detection in continuous waveform data using template matching",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/stefanocampanella/correlation-detector",
+    url="https://github.com/stefanocampanella/EQTM",
     packages=setuptools.find_packages(),
     scripts=['bin/eqtm-scan', 'bin/eqtm-avro2parquet', 'bin/eqtm-parquet2legacy', 'bin/eqtm-filter', 'bin/eqtm-select'],
     classifiers=[
@@ -20,17 +20,16 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
+        'obspy',
         'numpy',
         'scipy',
         'pandas',
         'bottleneck',
-        'tqdm',
-        'psutil',
         'fastavro',
         'python-snappy',
-        'obspy',
-        'xgboost',
         'fastparquet'
+        'tqdm',
+        'psutil',
     ],
     extras_require={'GPU acceleration': ['cupy']},
     include_package_data=True,
